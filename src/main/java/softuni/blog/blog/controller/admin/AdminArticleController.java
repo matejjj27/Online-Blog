879d0262e287746235aee0c8b53060f00417d892
Matej Kostov <matej4o9@gmail.com>
2022-06-10 09:30:49 +0300

    All articles Page for Admin #3

---
+++ src/main/java/softuni/blog/blog/controller/admin/AdminArticleController.java
@@ -0,0 +1,48 @@
+package softuni.blog.blog.controller.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import softuni.blog.blog.bindingModel.ArticleBindingModel;
+import softuni.blog.blog.entity.Article;
+import softuni.blog.blog.entity.Category;
+import softuni.blog.blog.entity.Tag;
+import softuni.blog.blog.repository.ArticleRepository;
+import softuni.blog.blog.repository.CategoryRepository;
+import softuni.blog.blog.repository.TagRepository;
+import softuni.blog.blog.repository.UserRepository;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/admin/article")
+public class AdminArticleController {
+
+    @Autowired
+    ArticleRepository articleRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
+    @Autowired
+    TagRepository tagRepository;
+
+    @GetMapping("/")
+    public String listArticles(Model model){
+
+        List<Article> articles = this.articleRepository.findAll();
+
+        model.addAttribute("articles", articles);
+        model.addAttribute("view", "/admin/article/list");
+
+        return "base-layout";
+    }
+
+
+}
